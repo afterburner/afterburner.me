0dbc83c04762e6828de321200bb9894e8bdc0e90
Austen McDonald <austenmc@fb.com>
2014-06-22 22:28:37 -0700

    Persist application submissions

--- web.rb
+++ web.rb
@@ -47,24 +47,49 @@ module Afterburner
       erb :apply
     end
 
-    get '/apply/2014-Q3' do
-      authenticate!
-
-      @current_session = Session.where(name: "2014-Q3").first
-
-#      prev = Submission.where(github_login: github_user.login,
-#                              session: s).first
-
-      @current_challenge = Challenge.where(name: "MovieSuggestions").first
-
+    def current_application
       @repos = github_user.api.repositories
 
       erb :current
     end
 
+    post '/apply/2014-Q3' do
+      authenticate!
+
+      form do
+        field :repo, :present => true
+        field :project_description, :present => true
+        field :t_shirt_size, :present => true, :regexp => %r{S|M|L|XL|XXL}
+      end
+      if form.failed?
+        current_application
+      else
+        s = Session.where(name: "2014-Q3").first
+        c = Challenge.where(name: "MovieSuggestions").first
+        Submission.create(github_login: github_user.login,
+                          repo: form[:repo],
+                          project_description: form[:project_description],
+                          t_shirt_size: form[:t_shirt_size],
+                          challenge: c,
+                          session: s)
+
+        redirect '/apply/thanks'
+      end
+    end
+
+    get '/apply/2014-Q3' do
+      authenticate!
+
+      current_application
+    end
+
     get '/auth/logout' do
       logout!
       redirect '/apply'
+    end
+
+    get '/apply/thanks' do
+      erb :apply_thanks
     end
   end
 end
