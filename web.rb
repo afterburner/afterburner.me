8a65a5f3072624b1f779c55e97b7e0fe870584d9
Austen McDonald <austenmc@fb.com>
2014-10-13 06:07:01 -0700

    Refactor data model and update application method

--- web.rb
+++ web.rb
@@ -7,25 +7,73 @@ require 'sinatra/formkeeper'
 Mongoid.load!("mongoid.yml")
 
 module Afterburner
+  class Permission
+    include Mongoid::Document
+    field :slug, type: String
+    field :name, type: String
+  end 
+
+  class User
+    include Mongoid::Document
+    include Mongoid::Timestamps
+    field :github_login, type: String
+    field :name, type: String
+    field :email, type: String
+    field :type, type: Symbol
+    field :t_shirt_size, type: String
+    has_many :applications
+    has_and_belongs_to_many :permissions, inverse_of: nil
+    has_many :decorations
+  end
+
   class Session
     include Mongoid::Document
+    field :slug, type: String
     field :name, type: String
+    field :start, type: Time
+    field :end, type: Time
+    field :apply_start, type: Time
+    field :apply_end, type: Time
+    has_one :challenge
+    has_many :applications
+  end
+
+  class Medal
+    include Mongoid::Document
+    include Mongoid::Timestamps
+    field :name, type: String
+    field :image, type: String
+    field :image_disabled, type: String
+    field :points, type: Integer
+    field :sort_key, type: String
+    field :description, type: String
+    field :secret, type: Boolean
+    has_many :decorations
+  end
+
+  class Decoration
+    include Mongoid::Document
+    include Mongoid::Timestamps
+    belongs_to :user
+    belongs_to :medal
   end
 
   class Challenge
     include Mongoid::Document
+    include Mongoid::Timestamps
+    field :slug, type: String
     field :name, type: String
+    belongs_to :session
   end
 
-  class Submission
+  class Application 
     include Mongoid::Document
+    include Mongoid::Timestamps
     field :github_login, type: String
     field :repo, type: String
     field :project_description, type: String
-    field :t_shirt_size, type: String
-    field :created_at, type: Time, default: ->{ Time.now }
-    embeds_one :session
-    embeds_one :challenge
+    belongs_to :user
+    belongs_to :session
   end
 
   class Website < Sinatra::Application
@@ -44,48 +92,37 @@ module Afterburner
       erb :index
     end
 
-    get '/application' do
-      redirect '/apply'
-    end
-
-    get '/apply' do
-      erb :apply
-    end
-
     def current_application
       @repos = github_user.api.repositories
 
       erb :current
     end
 
-    post '/apply/2014-Q3' do
+    post '/apply/:session_slug' do
       authenticate!
 
+      # Validate the session they're applying for.
+      s = Session.where(slug: params[:session_slug]).first
+      time = Time.now
+      if s.nil? || time < s.apply_start || time > s.apply_end
+        current_application
+      end
+
+      # Validate the form input.
       form do
         field :repo, :present => true
         field :project_description, :present => true
-        field :t_shirt_size, :present => true, :regexp => %r{S|M|L|XL|XXL}
       end
       if form.failed?
         current_application
       else
-        s = Session.where(name: "2014-Q3").first
-        c = Challenge.where(name: "MovieSuggestions").first
-        Submission.create(github_login: github_user.login,
-                          repo: form[:repo],
-                          project_description: form[:project_description],
-                          t_shirt_size: form[:t_shirt_size],
-                          challenge: c,
-                          session: s)
+        Application.create(github_login: github_user.login,
+                           repo: form[:repo],
+                           project_description: form[:project_description],
+                           session: s)
 
         redirect '/apply/thanks'
       end
-    end
-
-    get '/apply/2014-Q3' do
-      authenticate!
-
-      current_application
     end
 
     get '/auth/logout' do
