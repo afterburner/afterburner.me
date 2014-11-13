971bfe134ec1ab440589598d51fbc1d8d2ac6be5
Austen McDonald <austenmc@fb.com>
2014-11-12 20:19:08 -0800

    Add mentor leaderboard

--- web.rb
+++ web.rb
@@ -252,23 +252,23 @@ module Afterburner
     get '/leaderboard' do
       users = Afterburner::Users.all
       @cadet_leaders = []
+      @mentor_leaders = []
 
       # TODO: cache these in memory for a while
       users.each { |u|
-        if (u.type != :cadet)
-          next
-        end
-
         points = 0
         for d in u.decorations
           points += d.medal.points
         end
-        @cadet_leaders << { points: points, user: u }
+        if (u.type == :cadet)
+          @cadet_leaders << { points: points, user: u }
+        else
+          @mentor_leaders << { points: points, user: u }
+        end
       }
 
-      @cadet_leaders.sort! { |a,b|
-        b[:points] <=> a[:points]
-      }
+      @cadet_leaders.sort! { |a,b| b[:points] <=> a[:points] }
+      @mentor_leaders.sort! { |a,b| b[:points] <=> a[:points] }
       erb :leaderboard
     end
 
