ab02e5e3886022c7dd3e0e34e9330cad5f6c593a
Austen McDonald <austenmc@fb.com>
2014-11-12 22:42:29 -0800

    Move User model definition back to model.rb

--- afterburner/model.rb
+++ afterburner/model.rb
@@ -1,4 +1,17 @@
 module Afterburner
+  class User
+    include Mongoid::Document
+    include Mongoid::Timestamps
+    field :github_login, type: String
+    field :name, type: String
+    field :email, type: String
+    field :type, type: Symbol
+    field :t_shirt_size, type: String
+    has_many :applications
+    has_and_belongs_to_many :permissions, inverse_of: nil
+    has_many :decorations
+  end
+  
     class Permission
       include Mongoid::Document
       field :slug, type: String
--- afterburner/user.rb
+++ afterburner/user.rb
@@ -1,16 +1,5 @@
 module Afterburner
   class User
-    include Mongoid::Document
-    include Mongoid::Timestamps
-    field :github_login, type: String
-    field :name, type: String
-    field :email, type: String
-    field :type, type: Symbol
-    field :t_shirt_size, type: String
-    has_many :applications
-    has_and_belongs_to_many :permissions, inverse_of: nil
-    has_many :decorations
-
     def self.find(github_login)
       self.where(github_login: github_login).first
     end
