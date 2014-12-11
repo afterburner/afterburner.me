7ff55bed02aef97eebbebc36b05cb8a276a01767
Austen McDonald <austenmc@fb.com>
2014-12-10 23:55:10 -0800

    Add ability to accept/reject applications

--- routes/admin/applications.rb
+++ routes/admin/applications.rb
@@ -7,7 +7,8 @@ module Afterburner
       @applications = []
       for s in @sessions
         apps = Application.where(session: s)
-        if apps
+
+        if apps && apps.count > 0
           @applications << { :session => s, :applications => apps }
         end
       end
@@ -17,8 +18,28 @@ module Afterburner
       erb :admin_applications
     end
 
-    post '/admin/application/:application_id' do
+    post '/admin/application/:application_id/:action' do
       require!("applications_update")
+
+      application = Application.where(id: params[:application_id]).first
+
+      unless application
+        flash[:error] = "Couldn't find that application."
+        redirect '/admin/applications'
+      end
+
+      unless params[:action] =~ /accepted|rejected/
+        flash[:error] = "Invalid action on an application."
+        redirect '/admin/applications'
+      end
+
+      application.status = params[:action]
+      application.save
+
+      # TODO: check for errors here
+
+      flash[:message] = "Application updated."
+      redirect '/admin/applications'
     end
   end
 end
